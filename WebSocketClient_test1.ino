7c498d97a60e180a463f828268a2249fd5dbe2b5
koohm80 <98161918+koohm80@users.noreply.github.com>
2022-01-30 14:19:18 +0900

    Update WebSocketClient_test1.ino

--- WebSocketClient_test1.ino
+++ WebSocketClient_test1.ino
@@ -1,3 +1,20 @@
+/*
+  Esp8266 Websockets Client
+
+  This sketch:
+        1. Connects to a WiFi network
+        2. Connects to a Websockets server
+        3. Sends the websockets server a message ("Hello Server")
+        4. Prints all incoming messages while the connection is open
+
+  Hardware:
+        For this sketch you only need an ESP8266 board.
+
+  Created 15/02/2019
+  By Gil Maimon
+  https://github.com/gilmaimon/ArduinoWebsockets
+
+*/
 
 #include <ArduinoWebsockets.h>
 #include <ESP8266WiFi.h>
@@ -11,6 +28,11 @@ using namespace websockets;
 
 WebsocketsClient client;
 
+#define Relay_1_Engine 0
+#define Relay_2_BreakPedal 4
+
+int Relay_4_Engine_state = 0;
+
 void setup() {
     Serial.begin(115200);
     // Connect to wifi
@@ -21,7 +43,15 @@ void setup() {
         Serial.print(".");
         delay(1000);
     }
+    
     pinMode(LED_BUILTIN, OUTPUT); // LED 점등 테스트용
+    
+    pinMode(Relay_1_Engine, OUTPUT); // 릴레이 시동버튼 제어 출력    
+    pinMode(Relay_2_BreakPedal, OUTPUT); // 릴레이 시동버튼 제어 출력
+    
+    digitalWrite(Relay_1_Engine, HIGH);
+    digitalWrite(Relay_2_BreakPedal, HIGH);
+
     
     // Check if connected to wifi
     if(WiFi.status() != WL_CONNECTED) {
@@ -46,30 +76,27 @@ void setup() {
 
         //LED 점등 테스트용
         if (message.data() == "Engine_start/"){
-          digitalWrite(LED_BUILTIN, LOW);
+          Engine_Start(); //엔진시동 절차
         }
         if (message.data() == "Engine__stop/"){
-          digitalWrite(LED_BUILTIN, HIGH);
-        //LED 테스트용
+          Engine_Stop(); // 엔진 stop
         }
     });
 }
 
 unsigned long last_10sec = 0;
+unsigned long last_10sec2 = 0;
 unsigned int counter = 0;
+unsigned int counter2 = 0;
 String minute = "";
+String Start_Engine_seconds = "";
+
 void loop() {
     // let the websockets client check for incoming messages
     if(client.available()) {
         client.poll();
     }
-    
-    // 리커넥트 함수 관련 참고
-    // https://github.com/Links2004/arduinoWebSockets/blob/master/src/WebSocketsClient.cpp
-    
-    
-    
-    
+
     unsigned long t = millis();
     if((t - last_10sec) > 10 * 6000) { //지정 시간 마다 브로드캐스트 테스트 1000은 1초
         counter++;
@@ -78,5 +105,71 @@ void loop() {
         last_10sec = millis();
     }
 
+    unsigned long t2 = millis();
+    if((t2 - last_10sec2) > 10 * 1000 && digitalRead(LED_BUILTIN) == LOW){ // 시간 지정
+       Start_Engine_seconds = "";
+       counter2++;
+       Start_Engine_seconds = "VERACRUZ : Start Engine, " + (String)counter2 + " *10 seconds";
+       client.send(Start_Engine_seconds);
+       last_10sec2 = millis();
+    } else if(!digitalRead(LED_BUILTIN) == LOW){
+        counter2= 0;
+    }
+
 //    delay(500);
+}
+
+void Engine_Start(){
+
+ // 추가할 것
+ // 현재 시동온오프 상태 체크 후 오프이면 시동 절차 시작하기. 예정
+    if(!digitalRead(LED_BUILTIN) == LOW){
+    
+      delay(300);
+      client.send("VERACRUZ : Start btn 1");
+      digitalWrite(Relay_1_Engine, LOW);     //-------------------
+      delay(300);                             // 시동버튼 1회
+      digitalWrite(Relay_1_Engine, HIGH);      //-------------------
+      delay(3000);
+
+      client.send("VERACRUZ : Start btn 2");
+      digitalWrite(Relay_1_Engine, LOW);     //-------------------
+      delay(300);                             // 시동버튼 2회
+      digitalWrite(Relay_1_Engine, HIGH);      //-------------------
+      delay(4000);                             
+
+      client.send("VERACRUZ : Break Pedal On");
+      digitalWrite(Relay_2_BreakPedal, LOW); // 브레이크 밟고
+      delay(500);
+
+      client.send("VERACRUZ : Start btn 3");
+      digitalWrite(Relay_1_Engine, LOW);     //-------------------
+      delay(300);                             // 시동버튼 누름 = 엔진시동 ㄱㄱ
+      digitalWrite(Relay_1_Engine, HIGH);      //-------------------
+      delay(1000);
+    
+      client.send("VERACRUZ : Break Pedal Off");
+      digitalWrite(Relay_2_BreakPedal, HIGH);  // 브레이크 떼고
+
+      client.send("VERACRUZ : ### Start Engine!! ###");
+      digitalWrite(LED_BUILTIN, LOW);
+    } else if(digitalRead(LED_BUILTIN) == LOW){
+        client.send("VERACRUZ : engine running");
+        // 켜져 있으면 클라이언트에게 엔진시동 명령어 브로드캐스트 하게...
+    }
+}
+void Engine_Stop(){
+  if(digitalRead(LED_BUILTIN) == LOW){
+  
+    delay(500);
+    client.send("VERACRUZ : Stop btn 1");
+    digitalWrite(Relay_1_Engine, LOW);     //-------------------
+    delay(300);                             // 시동버튼 눌러 엔진정지
+    digitalWrite(Relay_1_Engine, HIGH);      //------------------- 
+
+    client.send("VERACRUZ : ### Stop Engine!! ###");
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else if(!digitalRead(LED_BUILTIN) == LOW){
+        client.send("VERACRUZ : engine not running");
+  }
 }
